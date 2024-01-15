7e20c440efa6caba235cb928846b707a732cd6f0
Hassan1228 <126410471+Hassan1228@users.noreply.github.com>
2024-01-16 01:58:40 +0500

    Update index.jsx

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import HeaderOne from '@/src/layout/headers/header-3';
+import  HeaderThree from '@/src/layout/headers/header-3';
 import Breadcrumb from '@/src/common/breadcrumb/breadcrumb';
 import AboutArea from './../homes/home-3/about-area';
 import CounterArea from '../homes/home-2/counter-area';
